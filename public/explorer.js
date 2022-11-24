543efecf67119cab5a176bd97c27db4fadde9256
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-11-24 17:42:25 -0600

    Update explorer.js

--- public/explorer.js
+++ public/explorer.js
@@ -978,6 +978,7 @@ async function getWatchList() {
       },
     });
     const data = await response.json();
+	  console.log(data)
     if (data.myWatchList.length > 0) {
       $(".watchlist").empty();
       $(".newUserMessage-watchlater message").css("display", "none");
